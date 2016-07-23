f4df29be21990d72e06fff3ac4471eaa248c268c
jerodcle <jeroen.declerck@ugent.be>
2016-07-23 21:00:40 +0200

    meet the team

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'sinatra/namespace'
 
 # dont require server restart when editing page content
 require 'sinatra/reloader' if development?
@@ -15,10 +16,18 @@ get '/faq' do
   erb :faq
 end
 
-get '/meetthecrew' do
-  erb :meetthecrew
-end
-
 get '/partners' do
   erb :partners
+end
+
+namespace '/meetthecrew' do
+  get { erb :meetthecrew }
+  get('/raadvanbestuur/?') { erb :'teams/raadvanbestuur' }
+  get('/teambeverages/?') { erb :'teams/teambeverages' }
+  get('/teamhr/?') { erb :'teams/teamhr' }
+  get('/teamit/?') { erb :'teams/teamit' }
+  get('/teampartners/?') { erb :'teams/teampartners' }
+  get('/teampr/?') { erb :'teams/teampr' }
+  get('/teamproductie/?') { erb :'teams/teamproductie' }
+  get('/teamsite/?') { erb :'teams/teamsite' }
 end
