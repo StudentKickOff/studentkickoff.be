1d859f73958a3f7607a4c65e8fae8c87509d9364
Lorin Werthen <werthenlorin@gmail.com>
2016-07-23 15:57:10 +0200

    make deployable for real

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@
 # You can define all roles on a single server, or split them:
 
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
-server 'artists.studentkickoff.be', user: 'artists', roles: %w(app db web)
+server 'artists.studentkickoff.be', user: 'beta', roles: %w(app db web)
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
