3bfc043516290b178f94cb1b43687e85bda8a059
Lorin Werthen <werthenlorin@gmail.com>
2016-08-05 09:08:25 +0200

    fix images

--- lib/filters/resize.rb
+++ lib/filters/resize.rb
@@ -4,13 +4,14 @@ class ResizeFilter < Nanoc::Filter
   identifier :resize
   type :binary
 
-  def run(filename, size: '90x90>', **params)
+  def run(filename, size: '90x90^', **params)
     puts "processing image #{filename}"
     img = MiniMagick::Image.open(filename)
     img.resize(size)
     img.format('jpg')
     img.gravity('Center')
-    img.crop '90x90<'
+    img.crop '90x90+0+0'
+    # img.repage
     img.write(output_filename)
   end
 end
