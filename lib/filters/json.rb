f26fd41288bdc3d84690d1342eaca2b596f8ed8e
Lorin Werthen <werthenlorin@gmail.com>
2016-09-20 13:44:57 +0200

    stuff

--- lib/filters/json.rb
+++ lib/filters/json.rb
@@ -1,5 +1,5 @@
 Nanoc::Filter.define(:json) do |content, content_identifier: :content, **h|
   {
     content_identifier => content
-  }.update(h).to_json.encode('Windows-1252').force_encoding('UTF-8')
+  }.update(h).to_json
 end
