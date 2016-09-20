f4724e6c02f433d53c25133479dfb95a25c127a2
Lorin Werthen <werthenlorin@gmail.com>
2016-09-20 17:46:49 +0200

    fix utf-8

--- lib/filters/json.rb
+++ lib/filters/json.rb
@@ -1,5 +1,13 @@
 Nanoc::Filter.define(:json) do |content, content_identifier: :content, **h|
   {
     content_identifier => content
-  }.merge(h).to_json
+  }.merge(h).to_json.encode('cp1252',
+                            fallback: {
+                              "\u0081" => "\x81".force_encoding('cp1252'),
+                              "\u008D" => "\x8D".force_encoding('cp1252'),
+                              "\u008F" => "\x8F".force_encoding('cp1252'),
+                              "\u0090" => "\x90".force_encoding('cp1252'),
+                              "\u009D" => "\x9D".force_encoding('cp1252')
+                            })
+    .force_encoding('utf-8')
 end
