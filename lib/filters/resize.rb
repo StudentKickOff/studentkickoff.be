7edfd89c6e69ba890b296efd3064847fbcab777e
Lorin Werthen <werthenlorin@gmail.com>
2017-04-02 16:51:45 +0200

    pagespeed

--- lib/filters/resize.rb
+++ lib/filters/resize.rb
@@ -7,13 +7,19 @@ class ResizeFilter < Nanoc::Filter
   def run(filename, size: '150x150^', **params)
     puts "processing image #{filename}"
     img = MiniMagick::Image.open(filename)
+
+    # Cropping
     img.resize(size)
-    img.format('jpg')
     img.gravity('Center')
     img.crop '150x150+0+0'
-    img.compress 'JPEG'
-    img.quality '75'
-    # img.repage
+
+    # Compressing
+    img.sampling_factor '4:2:0'
+    img.strip
+    img.quality '85'
+    img.interlace 'JPEG'
+    img.colorspace 'RGB'
+
     img.write(output_filename)
   end
 end
